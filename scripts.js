370ff1effb5a894a9cf9dfa5f4155e0c719e8bf4
MarianaReis1 <65280591+MarianaReis1@users.noreply.github.com>
2020-07-21 20:40:31 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const totalTime = 8000;
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
-window.onload = breatheAnimation();
+
 
 function breatheAnimation (){
     text.innerHTML = 'Breath In';
@@ -21,3 +21,6 @@ function breatheAnimation (){
 }
 
 setInterval(breatheAnimation, totalTime)
+
+
+window.onload = breatheAnimation();
